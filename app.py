c295e56d0278b558b54210bbb20562625f59c4f2
190330111 <87190773+190330111@users.noreply.github.com>
2021-08-24 13:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app=Flask(__name__,template_folder="templates")
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    if request.method=="GET":
+    if request.method=="POST":
        data={}
        data["got"]=True
        pr=request.form["Pregnancies"]
